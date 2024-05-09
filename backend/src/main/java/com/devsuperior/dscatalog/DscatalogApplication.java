e21a8291817973cfa00d46e2ebe678feb7b8e5be
jvmaiaa <joaovictormaia482@gmail.com>
2024-05-09 03:35:20 -0300

    Adiciona ModelMapper

--- backend/src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
+++ backend/src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dscatalog;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,10 +10,10 @@ import org.springframework.ui.ModelMap;
 public class DscatalogApplication {
 
 
-//	@Bean
-//	public ModelMapper modelMapper(){
-//		return new ModelMapper();
-//	}
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DscatalogApplication.class, args);
 	}
